8fed932fbdf93b897759cfeb7671f5ceafdcb765
David Nguyen <46098281+0x6769616E676E68@users.noreply.github.com>
2019-11-27 15:22:17 +0700

    Update migration (#3)

---
+++ database/migrations/2019_11_27_075915_create_slider_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSliderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slider', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->bigInteger('product_id');
+            $table->bigInteger('shop_id');
+            $table->timestamps();
+             $table->index('shop_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slider');
+    }
+}
