220ff668679b2bba0701bef79e7ca1e2f3acb452
thoitranyw <thoitq@fireapps.vn>
2019-11-27 22:06:35 +0700

    fix

--- resources/js/components/dashboard.js
+++ resources/js/components/dashboard.js
@@ -15,67 +15,67 @@ const DashboardComponent = () => {
             <Layout>
                 <Sidebar />
                 <Content style={{ padding: '24px', minHeight: 280 }}>
-                    <div class="dashboard-wrap">
-                        <div class="dashboard-header">
+                    <div className={'dashboard-wrap'}>
+                        <div className={'dashboard-header'}>
                             <p>Product funnel</p>
                         </div>
-                        <div class="dashboard-chart">
-                            <div class="dashboard-chart-percent__col">
-                                <p class="percent-text">100% of all</p>
-                                <div class="percent-col">
+                        <div className={'dashboard-chart'}>
+                            <div className={'dashboard-chart-percent__col'}>
+                                <p className={'percent-text'}>100% of all</p>
+                                <div className={'percent-col'}>
                                     <Icon type="eye" />
                                 </div>
-                                <p class="chart-title">Viewed Product</p>
-                                <p class="chart-statistic-number">2954</p>
+                                <p className={'chart-title'}>Viewed Product</p>
+                                <p className={'chart-statistic-number'}>2954</p>
                             </div>
 
-                            <div class="dashboard-chart-percent__line">
-                                <p class="percent-line__text">Completion rate</p>
-                                <div class="percent-line__number">
+                            <div className={'dashboard-chart-percent__line'}>
+                                <p className={'percent-line__text'}>Completion rate</p>
+                                <div className={'percent-line__number'}>
                                     29%
                                 </div>
                             </div>
 
-                            <div class="dashboard-chart-percent__col">
-                                <p class="percent-text">100% of all</p>
-                                <div class="percent-col">
+                            <div className={'dashboard-chart-percent__col'}>
+                                <p className={'percent-text'}>100% of all</p>
+                                <div className={'percent-col'}>
                                     <Icon type="shopping-cart" />
                                 </div>
-                                <p class="chart-title">Added product to cart</p>
-                                <p class="chart-statistic-number">2954</p>
+                                <p className={'chart-title'}>Added product to cart</p>
+                                <p className={'chart-statistic-number'}>2954</p>
                             </div>
 
-                            <div class="dashboard-chart-percent__line">
-                                <p class="percent-line__text">Completion rate</p>
-                                <div class="percent-line__number">
+                            <div className={'dashboard-chart-percent__line'}>
+                                <p className={'percent-line__text'}>Completion rate</p>
+                                <div className={'percent-line__number'}>
                                     29%
                                 </div>
                             </div>
 
-                            <div class="dashboard-chart-percent__col">
-                                <p class="percent-text">100% of all</p>
-                                <div class="percent-col">
+                            <div className={'dashboard-chart-percent__col'}>
+                                <p className={'percent-text'}>100% of all</p>
+                                <div className={'percent-col'}>
                                     <Icon type="credit-card" />
                                 </div>
-                                <p class="chart-title">Placed an order</p>
-                                <p class="chart-statistic-number">2954</p>
+                                <p className={'chart-title'}>Placed an order</p>
+                                <p className={'chart-statistic-number'}>2954</p>
                             </div>
                         </div>
-                        <div class="dashboard-statistic">
-                            <div class="dashboard-statistic__percent">
-                                <p class="dashboard-statistic--title">Completion rate</p>
-                                <p class="dashboard-statistic--number">15%</p>
+                        <div className={'dashboard-statistic'}>
+                            <div className={'dashboard-statistic__percent'}>
+                                <p className={'dashboard-statistic--title'}>Completion rate</p>
+                                <p className={'dashboard-statistic--number'}>15%</p>
                             </div>
-                            <div class="dashboard-statistic__time">
-                                <p class="dashboard-statistic--title">Avg complete time</p>
-                                <p class="dashboard-statistic--number">6:20 min</p>
+                            <div className={'dashboard-statistic__time'}>
+                                <p className={'dashboard-statistic--title'}>Avg complete time</p>
+                                <p className={'dashboard-statistic--number'}>6:20 min</p>
                             </div>
-                            <div class="dashboard-statistic__people">
-                                <p class="dashboard-statistic--title">Sample size</p>
-                                <p class="dashboard-statistic--number">2,954 people</p>
+                            <div className={'dashboard-statistic__people'}>
+                                <p className={'dashboard-statistic--title'}>Sample size</p>
+                                <p className={'dashboard-statistic--number'}>2,954 people</p>
                             </div>
                         </div>
-                        <div class="dashboard-table">
+                        <div className={'dashboard-table'}>
 
                         </div>
                     </div>
