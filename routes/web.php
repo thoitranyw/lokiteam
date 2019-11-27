0799d173f0943d070e88d66843b9d303067c324d
Nguyen Thai Sang <thaisang.nguyen3894@gmail.com>
2019-11-27 15:13:39 +0700

    add webhook controller (#4)

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,25 @@ Route::group(['prefix' => 'auth'], function () {
     Route::get('logout','AppsController@logout')->name('apps.logout');
 });
 
+
+//WebHook middleware
+Route::group(['middleware' => [], 'prefix' => 'webhook'], function() {
+	Route::post('app_uninstalled', 'WebHookController@uninstallApp')->name('webhook.uninstall_app');
+    Route::post('shop_update', 'WebHookController@shopUpdate')->name('webhook.shop_update');
+	// Route::post('orders_paid', 'WebHookController@ordersPaid')->name('webhook.orders_paid');
+	// Route::post('orders_updated', 'WebHookController@ordersUpdated')->name('webhook.orders_updated');
+	Route::post('created_product', 'WebHookController@createdProduct')->name('webhook.create_product');
+	Route::post('updated_product', 'WebHookController@updatedProduct')->name('webhook.update_product');
+	// Route::post('delete_product', 'WebHookController@deleteProduct')->name('webhook.delete_product');
+	// Route::post('update_fulfillments', 'WebHookController@updateFulfillments')->name('webhook.update_fulfillments');
+    // Route::post('create_fulfillments', 'WebHookController@createFulfillments')->name('webhook.create_fulfillments');
+    // Route::post('customers_redact', 'WebHookController@customersRedact')->name('webhook.customers_redact');
+    // Route::post('shop_redact', 'WebHookController@shopRedact')->name('webhook.shop_redact');
+    // Route::get('refunds_create', 'WebHookController@refundsCreate')->name('webhook.refunds_create');
+    // Route::get('orders_fulfilled', 'WebHookController@ordersFulfilled')->name('webhook.orders_fulfilled');
+    // Route::get('orders_partially_fulfilled', 'WebHookController@ordersPartiallyFulfilled')->name('webhook.orders_partially_fulfilled');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
