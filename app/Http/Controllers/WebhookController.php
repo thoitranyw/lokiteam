ddbc56a0500c202f9abbb8e87bc880ce6aa1be42
Nguyen Thai Sang <thaisang.nguyen3894@gmail.com>
2019-11-27 17:14:58 +0700

    add webhook for product and order (#10)

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repository\ShopRepository;
 use App\Models\ProductModel;
+use App\Models\ShopModel;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Arr;
 
 class WebhookController extends Controller
 {
@@ -25,16 +27,21 @@ class WebhookController extends Controller
     {
 	    $data = $request->all();
         $product = ProductModel::find($data['id']);
-        if(!$product) {
-            $product = new ProductModel();
-            if($product->save($data)) {
+        $shop_name = $request->server('HTTP_X_SHOPIFY_SHOP_DOMAIN');
+        $objectShop = ShopModel::where('myshopify_domain', $shop_name)->first();
+        $productModel = new ProductModel();
+        $filterData = Arr::only($data, $productModel->getFillable());
+        $filterData['shop_id'] = $objectShop->id;
+        if($product = $productModel->find($data['id'])) {
+            if($product->update($filterData)) {
                 return response()->json(['status' => true]);
             }
             return response()->json(['status' => false]);
         }
-        if($product::update($data))
-            return response()->json(['status' => true]);
 
+        if($productModel->firstOrCreate($filterData)) {
+            return response()->json(['status' => true]);
+        }
         return response()->json(['status' => false]);
     }
 
@@ -42,15 +49,31 @@ class WebhookController extends Controller
     {
         $data = $request->all();
         $product = ProductModel::find($data['id']);
-        if(!$product) {
-            if(ProductModel::create($data)) {
+        $shop_name = $request->server('HTTP_X_SHOPIFY_SHOP_DOMAIN');
+        $objectShop = ShopModel::where('myshopify_domain', $shop_name)->first();
+        $productModel = new ProductModel();
+        $filterData = Arr::only($data, $productModel->getFillable());
+        $filterData['shop_id'] = $objectShop->id;
+        if($product = $productModel->find($data['id'])) {
+            if($product->update($filterData)) {
                 return response()->json(['status' => true]);
             }
             return response()->json(['status' => false]);
         }
-        if($product->update($data))
-            return response()->json(['status' => true]);
 
+        if($productModel->firstOrCreate($filterData)) {
+            return response()->json(['status' => true]);
+        }
         return response()->json(['status' => false]);
+    }
+
+    public function ordersUpdated(Request $request)
+    {
+        $data = $request->all();
+        $lineItems = $data['line_items'];
+        foreach($lineItems as $lineItem) {
+            ProductModel::where('id', $lineItem['product_id'])->increment('checkout');
+        }
+        return response()->json(['status' => true]);
     }
 }
